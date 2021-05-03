26f6d4db0382ab816c7ba80fb0d52824b1943338
Atreyiu <jonathanremymichel@laposte.net>
2021-05-03 23:11:27 +0200

    correctif erreur blog/

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def blog(request):
-    return render(request, 'blog/portfolio.html')
+    return render(request, 'blog/blog.html')
 
 def blog_detail(request):
     return render(request, 'blog/blog_detail.html')
